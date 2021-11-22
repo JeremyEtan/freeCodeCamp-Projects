5c7439ca39ff343cc973c3e93f8feb79670683d0
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2021-11-22 05:08:33 +0100

    completed

--- JavaScript Algorithms and Data Structures/palindromeChecker.js
+++ JavaScript Algorithms and Data Structures/palindromeChecker.js
@@ -1,8 +1,8 @@
 function palindrome(str) {
-    var newstr = str.replace(/[\W_]/g, '').toLowerCase();  
-    return newstr === newstr.split('').reverse().join('');
+  var newstr = str.replace(/[\W_]/g, '').toLowerCase();  
+  return newstr === newstr.split('').reverse().join('');
 }
 
-console.log(palindrome("0_0 (: /-\ :) 0-0")); // should return true.
-console.log(palindrome("My age is 0, 0 si ega ym.")); // should return true
-console.log(palindrome("five|\_/|four")); // should return false
+palindrome("0_0 (: /-\ :) 0-0"); // should return true.
+palindrome("My age is 0, 0 si ega ym."); // should return true
+palindrome("five|\_/|four"); // should return false
